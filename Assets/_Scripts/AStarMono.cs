afca897885c74bd37f74d637cfa19f4c041ead4d
HBock <hugobock42@gmail.com>
2018-03-27 00:17:43 +0200

    Algorithm is working - Live display is working

--- Assets/_Scripts/AStarMono.cs
+++ Assets/_Scripts/AStarMono.cs
@@ -16,6 +16,7 @@ public class AStarMono : MonoBehaviour
 	[Header("Links")]
 	[SerializeField] private Transform _worldSpawn;
 	[SerializeField] private GameObject _cubePrefab;
+	[SerializeField] private GameObject _targetGo;
 
 	[Header("Links/Materials")]
 	[SerializeField] private Material _cubeGrey;
@@ -25,12 +26,15 @@ public class AStarMono : MonoBehaviour
 
 	private int[,] _map;
 	private Material[,] _mapCubesMaterials; // We use this array to colorize the possible path while the algorithm is running
+	private GameObject[,] _mapCubes; // We use this array to colorize the possible path while the algorithm is running
 	private Vector3 _cubeDimensions;
 	private Vector3 _centerTranslation;
+	private Dictionary<string, Vector2> _hashVectorCorrespondance = new Dictionary<string, Vector2>();
 
 	private void Start()
 	{
 		WorldBuilder();
+		StartCoroutine(AStar());
 	}
 
 	#region World Builder
@@ -53,6 +57,7 @@ public class AStarMono : MonoBehaviour
 		// Initialized to default value = 0;
 		_map = new int[_height, _width];
 		_mapCubesMaterials = new Material[_height, _width];
+		_mapCubes = new GameObject[_height, _width];
 
 		if (_start.x < 0) _start.x = 0;
 		if (_start.y < 0) _start.y = 0;
@@ -74,8 +79,16 @@ public class AStarMono : MonoBehaviour
 				GameObject temp = Instantiate(_cubePrefab, _worldSpawn);
 				temp.transform.position = new Vector3(x * _cubeDimensions.x, -_cubeDimensions.y + _cubeDimensions.y / 2, y * _cubeDimensions.z) - _centerTranslation;
 				_mapCubesMaterials[y, x] = temp.GetComponent<MeshRenderer>().material;
+				_mapCubes[y, x] = temp;
 
-				if ((Math.Abs(x - _start.x) < 0.05f && Math.Abs(y - _start.y) < 0.05f) || (Math.Abs(x - _end.x) < 0.05f && Math.Abs(y - _end.y) < 0.05f)) continue;
+				if (x == Mathf.RoundToInt(_end.x) && y == Mathf.RoundToInt(_end.y))
+				{
+					temp = Instantiate(_targetGo, _worldSpawn);
+					temp.name = "Target";
+					temp.transform.position = _mapCubes[y, x].transform.position;
+					continue;
+				}
+				if ((Math.Abs(x - _start.x) < 0.05f && Math.Abs(y - _start.y) < 0.05f)) continue;
 
 				if (Random.Range(0.0f, 1.0f) >= _fillRatio) continue;
 
@@ -115,4 +128,216 @@ public class AStarMono : MonoBehaviour
 
 	#endregion
 
+
+	#region A* Algorithme
+
+	// https://en.wikipedia.org/wiki/A*_search_algorithm
+	IEnumerator AStar()
+	{
+		bool found = false;
+		List<Vector2> closedSet = new List<Vector2>();
+		List<Vector2> openSet = new List<Vector2> {_start};
+		List<Vector2> neighbors = new List<Vector2>();
+
+		Dictionary<string, string> cameFrom = new Dictionary<string, string>();
+		Dictionary<string, float> gScore = new Dictionary<string, float>();
+		gScore[HashVector(_start)] = 0;
+		Dictionary<string, float> fScore = new Dictionary<string, float>();
+		fScore[HashVector(_start)] = HeuristicCostEstimation(_start, _end);
+		_hashVectorCorrespondance[HashVector(_start)] = _start;
+
+		while (openSet.Count > 0)
+		{
+			Vector2 current = GetNodeWithBestFScore(openSet, fScore);
+
+			if (current.EqualRounded(_end))
+			{
+				Debug.Log("Path Found");
+				ColorCubes(closedSet, openSet, cameFrom, current);
+				found = true;
+				break;
+			}
+
+			openSet.Remove(current);
+			closedSet.Add(current);
+
+			GetNeighbors(ref neighbors, current);
+			for (int i = 0; i < neighbors.Count; i++)
+			{
+				if (IsInList(ref closedSet, neighbors[i])) continue;
+				if (!IsInList(ref openSet, neighbors[i])) openSet.Add(neighbors[i]);
+
+				if (!gScore.ContainsKey(HashVector(neighbors[i])))
+				{
+					gScore[HashVector(neighbors[i])] = float.PositiveInfinity;
+					_hashVectorCorrespondance[HashVector(neighbors[i])] = neighbors[i];
+				}
+				float tentativeGScore = gScore[HashVector(current)] + VectorDistance(current, neighbors[i]);
+				if (tentativeGScore >= gScore[HashVector(neighbors[i])]) continue;
+
+				cameFrom[HashVector(neighbors[i])] = HashVector(current);
+				gScore[HashVector(neighbors[i])] = tentativeGScore;
+				fScore[HashVector(neighbors[i])] = gScore[HashVector(neighbors[i])] + HeuristicCostEstimation(neighbors[i], _end);
+			}
+
+			ColorCubes(closedSet, openSet, cameFrom, current);
+			yield return new WaitForEndOfFrame();
+		}
+
+		if (!found)
+		{
+			Debug.Log("Nothing found");
+		}
+	}
+
+
+	/// <summary>
+	/// Create a Hash from a Vector2
+	/// x and y are cast into int, use this as if you were working with int Vectors
+	/// </summary>
+	/// <returns>A hash usable with dictionary</returns>
+	private string HashVector(Vector2 pVector)
+	{
+		return Mathf.RoundToInt(pVector.x) + "-" + Mathf.RoundToInt(pVector.y);
+	}
+
+	/// <summary>
+	/// Calcul the estimated cost to go from pFrom to pTarget
+	/// </summary>
+	/// <returns>Return the estimated cost</returns>
+	private float HeuristicCostEstimation(Vector2 pFrom, Vector2 pTarget)
+	{
+		// This seems stupid but I want to keep the choice to change my heristic cost estimation later
+		return VectorDistance(pFrom, pTarget);
+	}
+
+	/// <summary>
+	/// World Distance between pFrom and pTarget
+	/// We take in account the _cubeDimensions calculated earlier
+	/// </summary>
+	/// <returns>The distance between pFrom and pTarget</returns>
+	private float VectorDistance(Vector2 pFrom, Vector2 pTarget)
+	{
+		Vector2 diff = pTarget - pFrom;
+		diff.x *= _cubeDimensions.x;
+		diff.y *= _cubeDimensions.z;
+		return diff.magnitude;
+	}
+
+	/// <summary>
+	/// Return the vector with the best fScore
+	/// </summary>
+	/// <param name="pOpenSet">List of vector we are comparing the fscores</param>
+	/// <param name="pFScore">fscore dictionnary</param>
+	/// <returns></returns>
+	private Vector2 GetNodeWithBestFScore(List<Vector2> pOpenSet, Dictionary<string, float> pFScore)
+	{
+		int bestId = 0;
+		for (int i = 0; i < pOpenSet.Count; i++)
+		{
+			if (pFScore[HashVector(pOpenSet[i])] < pFScore[HashVector(pOpenSet[bestId])])
+			{
+				bestId = i;
+			}
+		}
+
+		return pOpenSet[bestId];
+	}
+
+	/// <summary>
+	/// Get the neighbors of a vector2 (pCurrent)
+	/// </summary>
+	/// <param name="pNeighbors">List of neighbors, we fill it</param>
+	/// <param name="pCurrent">The Vector we are searching the neighbors</param>
+	private void GetNeighbors(ref List<Vector2> pNeighbors, Vector2 pCurrent)
+	{
+		pNeighbors.Clear();
+
+		Vector2 tmp;
+
+		tmp = pCurrent - Vector2.right;
+		if (Mathf.RoundToInt(tmp.x) >= 0 && _map[Mathf.RoundToInt(tmp.y), Mathf.RoundToInt(tmp.x)] == 0) pNeighbors.Add(tmp);
+		tmp = pCurrent + Vector2.right;
+		if (Mathf.RoundToInt(tmp.x) < _width && _map[Mathf.RoundToInt(tmp.y), Mathf.RoundToInt(tmp.x)] == 0) pNeighbors.Add(tmp);
+		tmp = pCurrent - Vector2.up;
+		if (Mathf.RoundToInt(tmp.y) >= 0 && _map[Mathf.RoundToInt(tmp.y), Mathf.RoundToInt(tmp.x)] == 0) pNeighbors.Add(tmp);
+		tmp = pCurrent + Vector2.up;
+		if (Mathf.RoundToInt(tmp.y) < _height && _map[Mathf.RoundToInt(tmp.y), Mathf.RoundToInt(tmp.x)] == 0) pNeighbors.Add(tmp);
+	}
+
+	/// <summary>
+	/// </summary>
+
+	/// <summary>
+	/// Function to determine if pToCheck is in pList
+	/// </summary>
+	/// <param name="pList">List to search into</param>
+	/// <param name="pToCheck">Vector to search for</param>
+	/// <returns>True if pToCheck is found, else false</returns>
+	private bool IsInList(ref List<Vector2> pList, Vector2 pToCheck)
+	{
+		foreach (Vector2 v1 in pList)
+		{
+			if (v1.EqualRounded(pToCheck)) return true;
+		}
+
+		return false;
+	}
+
+
+	private List<Vector2> GetPath(Dictionary<string, string> pCameFrom, Vector2 pCurrent)
+	{
+		List<Vector2> lPath = new List<Vector2>(){ pCurrent };
+		string hash = HashVector(pCurrent);
+		while (pCameFrom.ContainsKey(hash))
+		{
+			hash = pCameFrom[hash];
+			lPath.Insert(0, _hashVectorCorrespondance[hash]);
+		}
+
+		return lPath;
+	}
+
+	#endregion
+
+	#region Display
+
+	private void ColorCubes(List<Vector2> pClosedSet, List<Vector2> pOpenSet, Dictionary<string, string> pCameFrom, Vector2 pCurrent)
+	{
+		foreach (Vector2 v2 in pClosedSet)
+		{
+			_mapCubesMaterials[Mathf.RoundToInt(v2.y), Mathf.RoundToInt(v2.x)] = _cubeBlue;
+
+			_mapCubes[Mathf.RoundToInt(v2.y), Mathf.RoundToInt(v2.x)].GetComponent<MeshRenderer>().material = _cubeBlue;
+				
+		}
+
+		foreach (Vector2 v2 in pOpenSet)
+		{
+			_mapCubesMaterials[Mathf.RoundToInt(v2.y), Mathf.RoundToInt(v2.x)] = _cubeRed;
+
+			 _mapCubes[Mathf.RoundToInt(v2.y), Mathf.RoundToInt(v2.x)].GetComponent<MeshRenderer>().material = _cubeRed;
+		}
+
+		List<Vector2> currentPath = GetPath(pCameFrom, pCurrent);
+		foreach (Vector2 v2 in currentPath)
+		{
+			_mapCubesMaterials[Mathf.RoundToInt(v2.y), Mathf.RoundToInt(v2.x)] = _cubeGreen;
+
+			_mapCubes[Mathf.RoundToInt(v2.y), Mathf.RoundToInt(v2.x)].GetComponent<MeshRenderer>().material = _cubeGreen;
+		}
+	}
+
+	#endregion
+}
+
+public static class MyExtensions
+{
+	/// <summary>
+	/// Round the x and y component of the vectors pV1 and pV2 then make an equality comparision
+	/// </summary>
+	public static bool EqualRounded(this Vector2 pV1, Vector2 pV2)
+	{
+		return Mathf.RoundToInt(pV1.x) == Mathf.RoundToInt(pV2.x) && Mathf.RoundToInt(pV1.y) == Mathf.RoundToInt(pV2.y);
+	}
 }
